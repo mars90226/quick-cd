d12d14cd48f65b31338a1ddeeb0f1a572730436d
Mars Peng <mars.90226@gmail.com>
2017-05-06 10:44:38 +0800

    Fix bug that powershell version cannot add target

--- qcd.ps1
+++ qcd.ps1
@@ -3,7 +3,7 @@ $tempfile = "moveto.ps1"
 $directory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $path = Join-Path $directory $tempfile
 
-ruby (Join-Path $directory $script) $args[0] -p > $path
+ruby (Join-Path $directory $script) @args -p > $path
 if($LASTEXITCODE -eq 10) {
 	& $path
 } else {
